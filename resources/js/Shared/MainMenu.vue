73636911e37948b3e7e8d087061ae29c932b3a16
yxj0312 <yanxiajun@hotmail.com>
2021-03-12 22:41:37 +0100

    Add a route testing links

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -24,6 +24,13 @@
         <div :class="isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Reports</div>
       </inertia-link>
     </div>
+
+    <div class="mb-4">
+      <inertia-link class="flex items-center group py-3" :href="route('testing')">
+        <icon name="printer" class="w-4 h-4 mr-2" :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <div :class="isUrl('testing') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Testing</div>
+      </inertia-link>
+    </div>
   </div>
 </template>
 
